65c76b4c162c6904360d89711e912dc45166f61d
TengchuanWang <wangtengchuan@gmail.com>
2014-08-27 23:09:06 +0800

    show rgb

--- js/showrgb.js
+++ js/showrgb.js
@@ -1,11 +1,15 @@
-function showRGB(whichPic)
+var canvas;
+var ctx;
+$(function)()
 {
 	var canvas=document.getElemetById("myCanvas");
 	var ctx=canvas.getContext("2d");
-	var img=document.getElementById("imageSource");//the image source
+	var img=new Image();
+	img.src="./img/img.jpg"
+	//var img=document.getElementById("imageSource");//the image source
 	//TODO
 	ctx.drawImage(img,10,10);
-	myCanvas.click=function(e)//mouse click handler
+	$('myCanvas').click(function(e))//mouse click handler
 	{
 		var canvasOffset=canvas.offset();
 		var canvasX=Math.floor(e.pageX-canvasOffset.left);
