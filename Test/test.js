046b666d06e5966bad3b8e852523b756012d5e7e
TengchuanWang <wangtengchuan@gmail.com>
2014-08-27 17:23:43 +0800

    detach content from format

--- Test/test.js
+++ Test/test.js
@@ -1,3 +1,20 @@
+function addLoadEvent(func)
+{
+	var onload=window.onload;
+	if(typeof window.onload!='function')
+	{
+		window.onload=func;
+	}
+	else
+	{
+		window.onload=function()
+		{
+			oldonload();
+			func();
+		}
+	}
+}
+
 function showPic(whichPic)
 {
 	if(!document.getElementById("placeholder"))return true;
@@ -14,7 +31,19 @@ function showPic(whichPic)
 	}
 	return false;
 }
-window.open();
+
+function insertAfter(newElement,targetElement)
+{
+	var parent=targetElement.parentNode;
+	if(parent.lastChild==targetElement)
+	{
+		parent.appendChild(newElement);
+	}
+	else
+	{
+		parent.insertBefore(newElement,targetElement.nextSibling);
+	}
+}
 
 function prepareGallery()
 {
@@ -31,3 +60,24 @@ function prepareGallery()
 		}
 	}
 }
+
+function preparePlaceholder()
+{
+	if(!document.createElement)return false;
+	if(!document.createTextNode)return false;
+	if(!document.getElementById)return false;
+	if(!document.getElementById("imagegallery"))return false;
+	var placeholder=document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","images/placeholder.jpg");
+	placeholder.setAttribute("alt","my image gallery");
+	var description=document.createElement("p");
+	description.setAttribute("id","description");
+	var desctext=document.createTextNode("Choose an image");
+	description.appendChild(desctext);
+	var gallery=document.getElementById("imagegallery");
+	insertAfter(placeholder,gallery);
+	insertAfter(description,placeholder);
+}
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
