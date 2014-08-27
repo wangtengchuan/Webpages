ed652693119d88d0864dfc687255376fc0443c4e
TengchuanWang <wangtengchuan@gmail.com>
2014-08-27 13:40:47 +0800

    exception handle

--- Test/test.js
+++ Test/test.js
@@ -1,10 +1,33 @@
 function showPic(whichPic)
 {
+	if(!document.getElementById("placeholder"))return true;
 	var source=whichPic.getAttribute("href");
 	var placeholder=document.getElementById("placeholder");
+	if(placeholder.nodeName!="IMG")return true;
 	placeholder.setAttribute("src",source);
-	var titletext=whichPic.getAttribute("title");
+	if(!document.getElementById("description"))return false;
+	var titletext=whichPic.getAttribute("title")?whichPic.getAttribute("title"):"";
 	var description=document.getElementById("description");
-	description.firstChild.nodeValue=titletext;
+	if(description.firstChild.nodeType==3)//meaning the node is a text
+	{
+		description.firstChild.nodeValue=titletext;
+	}
+	return false;
 }
 window.open();
+
+function prepareGallery()
+{
+	if(!document.getElementsByTagName)return false;
+	if(!document.getElementById)return false;
+	if(!document.getElementById("imagegallery"))return false;
+	var gallery=document.getElementById("imagegallery");
+	var links=gallery.getElementsByTagName("a");
+	for(var i=0;i<links.length;i++)
+	{
+		links[i].onclick=function()
+		{
+			return showPic(this);
+		}
+	}
+}
