ee58c5ccc526cd586674bb926d359c7191731661
TengchuanWang <wangtengchuan@gmail.com>
2014-09-04 17:22:42 +0800

    add popup modal

--- js/websocket.js
+++ js/websocket.js
@@ -144,10 +144,10 @@ function processArrayBuffer(buf)
 
 
 /* blur on modal open, unblur on close */
-$('#myModal').on('show.bs.modal', function () {
+/*$('#myModal').on('show.bs.modal', function () {
    $('.container').addClass('blur');
 })
 
 $('#myModal').on('hide.bs.modal', function () {
    $('.container').removeClass('blur');
-})
+})*/
